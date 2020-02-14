774eabea18d0ecfe876088a2b94c4d0b442164ab
Z10N0110 <dungeoncog@outlook.com>
2020-02-14 18:16:29 +0800

    feat: rich info list methods & various helper methods

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # go-ipset
 
-[![Build Status](https://api.travis-ci.org/gmccue/go-ipset.png?branch=master)](https://travis-ci.org/gmccue/go-ipset)
-[![GoDoc](https://godoc.org/github.com/gmccue/go-ipset?status.svg)](https://godoc.org/github.com/gmccue/go-ipset)
+[![GoDoc](https://godoc.org/github.com/gmccue/go-ipset?status.svg)](https://godoc.org/github.com/Z10N0110/go-ipset)
 
 go-ipset provides basic bindings for the [ipset kernel utility](http://ipset.netfilter.org/).
 
 ## Installation
-```
-go get github.com/gmccue/go-ipset
+
+``` shell
+go get github.com/Z10N0110/go-ipset
 ```
 
 ## Usage
-The following are some basic usage examples for go-iptables. For more information, please [checkout the godoc](https://godoc.org/github.com/gmccue/go-ipset).
 
-```go
-import "github.com/gmccue/ipset"
+The following are some basic usage examples for go-iptables. For more information, please [checkout the godoc](https://github.com/Z10N0110/go-ipset.git).
+
+``` go
+import "github.com/Z10N0110/ipset"
 
 // Construct a new ipset instance
 ipset, err := ipset.New()
@@ -30,7 +31,8 @@ if err != nil {
 ```
 
 ### Adding an entry to an ipset
-```go
+
+``` go
 err := ipset.Add("my_set", "127.0.0.1")
 if err != nil {
     // Your custom error handling here.
@@ -38,14 +40,44 @@ if err != nil {
 ```
 
 ### Removing an entry from an ipset
-```go
+
+``` go
+err := ipset.Add("my_set", "127.0.0.1")
+if err != nil {
+    // Your custom error handling here.
+}
+```
+
+### Refresh named set with new entries
+
+``` go
+err := ipset.Refresh("my_set", "127.0.0.2", "192.168.1.1")
+if err != nil {
+    // Your custom error handling here.
+}
+```
+
+### List all sets names
+
+``` go
+names, err := ipset.ListSetNames()
+if err != nil {
+    // Your custom error handling here.
+}
+```
+
+### Get all entries of a named set
+
+``` go
+entries, err := ipset.ListEntries("my_set")
 if err != nil {
     // Your custom error handling here.
 }
 ```
 
 ### Save your ipset to a file
-```go
+
+``` go
 err := ipset.Save("my_set", "/tmp/my_set.txt")
 if err != nil {
     // Your custom error handling here.
@@ -53,6 +85,7 @@ if err != nil {
 ```
 
 ### Restore your ipset from a file
+
 ```go
 err := ipset.Restore("/tmp/my_set.txt")
 if err != nil {
--- ipset.go
+++ ipset.go
@@ -5,22 +5,27 @@ package ipset
 
 import (
 	"bytes"
+	"encoding/xml"
 	"errors"
+	"fmt"
+	"io"
 	"os/exec"
+	"strconv"
 )
 
+// IPSet represents a ipset cmd executor.
 type IPSet struct {
-	Path    string
-	Options []string
+	Path string
 }
 
+// New creates a new IPSet.
 func New() (*IPSet, error) {
 	binPath, err := exec.LookPath("ipset")
 	if err != nil {
 		return nil, err
 	}
 
-	return &IPSet{binPath, []string{}}, nil
+	return &IPSet{binPath}, nil
 }
 
 // Create creates a new ipset with a given name and type.
@@ -86,11 +91,157 @@ func (set *IPSet) Swap(from string, to string) error {
 	return set.run("swap", from, to)
 }
 
+// SetEntry ...
+type SetEntry struct {
+	XMLName xml.Name `xml:"member"`
+	Elem    string   `xml:"elem"`
+}
+
+// Set ...
+type Set struct {
+	XMLName  xml.Name `xml:"ipset"`
+	Name     string   `xml:"name,attr"`
+	Type     string   `xml:"type"`
+	Revision string   `xml:"revision"`
+	Header   struct {
+		XMLName    xml.Name `xml:"header"`
+		Family     string   `xml:"family"`
+		HashSize   int64    `xml:"hashsize"`
+		Maxelem    int64    `xml:"maxelem"`
+		MemSize    int64    `xml:"memsize"`
+		References int64    `xml:"references"`
+		Numentries int64    `xml:"numentries"`
+	} `xml:"header"`
+	Members struct {
+		XMLName xml.Name    `xml:"members"`
+		Members []*SetEntry `xml:"member"`
+	} `xml:"members"`
+}
+
+// Sets ...
+type Sets struct {
+	XMLName xml.Name `xml:"ipsets"`
+	Sets    []*Set   `xml:"ipset"`
+}
+
+func (set *IPSet) listXML(suppressMembers bool, args ...string) (sets []*Set, err error) {
+	var stdout bytes.Buffer
+	args = append([]string{"list", "-o", "xml"}, args...)
+	if suppressMembers {
+		args = append(args, "-t")
+	}
+	if err := set.runWithOutput(args, &stdout); err != nil {
+		return nil, err
+	}
+
+	s := new(Sets)
+	if err = xml.Unmarshal(stdout.Bytes(), s); err != nil {
+		return nil, err
+	}
+
+	return s.Sets, nil
+}
+
+// List shows the named set by unmarshal xml output.
+func (set *IPSet) List(name string, suppressMembers bool) (*Set, error) {
+	sets, err := set.listXML(suppressMembers, name)
+	if err != nil {
+		return nil, err
+	}
+	if len(sets) != 1 {
+		return nil, errors.New("list named set return results not equal one")
+	}
+	return sets[0], nil
+}
+
+// ListEntries shows the entries of a named set.
+func (set *IPSet) ListEntries(name string) (entries []string, err error) {
+	s, err := set.List(name, false)
+	if err != nil {
+		return
+	}
+	for _, m := range s.Members.Members {
+		entries = append(entries, m.Elem)
+	}
+	return
+}
+
+// ListSets returns a slice of each set.
+func (set *IPSet) ListSets(suppressMembers bool) (sets []*Set, err error) {
+	return set.listXML(suppressMembers)
+}
+
+// ListSetNames returns a slice containing the name of each set.
+func (set *IPSet) ListSetNames() (names []string, err error) {
+	sets, err := set.listXML(false, "-n")
+	if err != nil {
+		return
+	}
+	for _, s := range sets {
+		names = append(names, s.Name)
+	}
+	return
+}
+
+// GetReferences returns the named set's References
+func (set *IPSet) GetReferences(name string) (int64, error) {
+	s, err := set.List(name, true)
+	if err != nil {
+		return 0, err
+	}
+	return s.Header.References, nil
+}
+
+// Refresh use swap command to overwrite the set with the specified entries.
+func (set *IPSet) Refresh(name string, entries ...string) error {
+
+	tempname := name + "-swptemp"
+	var err error
+
+	s, err := set.List(name, true)
+	if err != nil {
+		return err
+	}
+
+	opts := []string{
+		"family", s.Header.Family,
+		"hashsize", strconv.FormatInt(s.Header.HashSize, 10),
+		"maxelem", strconv.FormatInt(s.Header.Maxelem, 10),
+	}
+	if err = set.Create(tempname, s.Type, opts...); err != nil {
+		msg := fmt.Sprintf("cannot create the temp set for swap: %s", err.Error())
+		return errors.New(msg)
+	}
+
+	for _, entry := range entries {
+		if err = set.AddUnique(tempname, entry); err != nil {
+			return err
+		}
+	}
+
+	if err = set.Swap(tempname, name); err != nil {
+		return err
+	}
+
+	if err = set.Destroy(tempname); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (set *IPSet) run(args ...string) error {
+	return set.runWithOutput(args, nil)
+}
+
+func (set *IPSet) runWithOutput(args []string, stdout io.Writer) error {
+	args = append([]string{set.Path}, args...)
+
 	var stderr bytes.Buffer
 	cmd := exec.Cmd{
 		Path:   set.Path,
-		Args:   append([]string{set.Path}, args...),
+		Args:   args,
+		Stdout: stdout,
 		Stderr: &stderr,
 	}
 
